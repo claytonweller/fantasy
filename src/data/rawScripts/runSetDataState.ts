e354f170490f9877d178afe1404bb57ce71d2f47
Clayton weller <clayton.weller@gmail.com>
2025-04-13 22:59:11 -0600

    Create setDataState function
    This will allow for a more human readable process

---
+++ src/data/rawScripts/runSetDataState.ts
@@ -0,0 +1,30 @@
+import { DataStates, setDataState } from "./setDataState";
+
+const INFINITE_FUTURE = 1000;
+
+let week = INFINITE_FUTURE;
+let dataState = DataStates.WeekComplete
+
+process.argv.forEach((arg, i) => {
+  const nextValue = process.argv[i + 1]
+  if (arg === "--week") {
+    week = parseInt(nextValue);
+    if (Number.isNaN(week)) week = INFINITE_FUTURE;
+  }
+  console.info("Setting data state to week: ", week);
+
+  if (arg === "--state") {
+    if(nextValue === 'quest-only'){
+      console.info("Only quests");
+      dataState = DataStates.QuestsOnly;
+    } else if(nextValue === 'before-points'){
+      console.info("Before points");
+      dataState = DataStates.BeforePoints;
+    } else {
+      console.info('Unknown stat defaulting to complete week')
+    }
+
+  }
+});
+
+setDataState(week, dataState);
---
+++ src/data/rawScripts/setDataState.ts
@@ -0,0 +1,19 @@
+import { scrubByWeek } from "./scrubByWeek";
+
+export function setDataState(week: number, dataState: DataStates){
+
+  if(dataState === DataStates.QuestsOnly){
+    return scrubByWeek({week, shouldScrubParties: true, shouldScrubMetrics: true})
+  }
+  if(dataState === DataStates.BeforePoints){
+    return scrubByWeek({week, shouldScrubMetrics: true})
+  }
+  return scrubByWeek({week})
+
+}
+
+export enum DataStates {
+  QuestsOnly = "QuestsOnly",
+  BeforePoints = "BeforePoints",
+  WeekComplete = "WeekComplete"
+}
