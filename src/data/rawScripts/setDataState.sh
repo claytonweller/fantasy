d8a2f63bcee2c479e1385a8f5ca502244bb0258a
Clayton weller <clayton.weller@gmail.com>
2025-04-14 00:33:16 -0600

    Add safety against double rollbacks

--- src/data/rawScripts/setDataState.sh
+++ src/data/rawScripts/setDataState.sh
@@ -1,3 +1,10 @@
+if [ -d "./src/data/snapshots/state-safety" ]; then
+  echo "Already in a rolled back data state."
+  echo "Cannot set a new state without destroying data."
+  echo "run 'yarn revert-data-state' first"
+  exit 1
+fi
+
 rm -rf ./src/data/snapshots/state-safety
 yarn store-snapshot state-safety
 
