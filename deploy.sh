1932425ebb2f760155915c7668eac777e7e029e7
Clayton weller <clayton.weller@gmail.com>
2025-04-14 00:21:23 -0600

    Pull All quests into main file

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ yarn build
 aws s3 sync build/ s3://fantasy.claytonweller.com --acl public-read
 # Push changes to hosting service
 aws cloudfront create-invalidation --no-cli-pager --distribution-id E8N78ZCJXLHBY --paths "/*"
-yarn set-snapshot deploy-safety
+yarn load-snapshot deploy-safety
 rm -rf ./src/data/snapshots/deploy-safety
 
---
+++ src/data/rawScripts/revertDataState.sh
@@ -0,0 +1,2 @@
+yarn load-snapshot state-safety
+rm -rf ./src/data/snapshots/state-safety
